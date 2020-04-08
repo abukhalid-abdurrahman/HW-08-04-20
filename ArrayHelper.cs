7ca648e53f04d418e4f12803ef7b542f8b0d0be0
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2020-04-08 16:53:38 +0300

    Add files via upload

--- ArrayHelper.cs
+++ ArrayHelper.cs
@@ -1,12 +1,25 @@
 using System;
+using System.Linq;
 
 namespace Day_9
 {
     public class ArrayHelper
     {
-        public static void Pop(ref Type<T> array)
+        public static string Pop<T>(ref T[] array)
         {
+            int endIndex = array.Length - 1;
+            string results = string.Empty;
+            if(array != null)
+            {
+                results = array[endIndex].ToString();
+                Array.Resize(ref array, array.Length - 1);
+            }
+            return results;
+        }
 
+        public static void Output(object[] array)
+        {
+            Console.WriteLine($"Elements of Array (Length: {array.Length}): " + String.Join(",", array));
         }
     }
 }

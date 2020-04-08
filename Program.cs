c4320b805535aa13de133def3b64fe691a0c75ce
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2020-04-08 14:46:20 +0300

    Тестирование метода Pop для string[]

--- Program.cs
+++ Program.cs
@@ -6,7 +6,11 @@ namespace Day_9
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Testing ArrayHelper Class (using string type)...");
+            string[] arrStr = {"Faridun", "Firuz", "Jamshed", "Kamila", "Nisso", "Khasan", "Mijgona"};
+            string deletedElement = ArrayHelper.Pop(ref arrStr);
+            Console.WriteLine("Deleted: " + deletedElement);
+            ArrayHelper.Output(arrStr);
         }
     }
 }
